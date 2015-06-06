cad6652d806fd92e0d34dcf665220aa056b63c7f
mambax7 <mambax7@gmail.com>
2015-06-06 05:40:21 -0400

    Original files

---
+++ whity/txc_themeinfo.php
@@ -0,0 +1,7 @@
+<?php
+	$txc_themeinfo["name"] = "Whity Theme";
+	$txc_themeinfo["author"] = " Konik Erol";
+	$txc_themeinfo["author_url"] = "http://www.pierdtimp.ro";
+	$txc_themeinfo["detail_url"] = "http://www.pierdtimp.ro/modules/wfdownloads/singlefile.php?cid=57&lid=4247";
+	$txc_themeinfo["download_url"] = "http://www.pierdtimp.ro/modules/wfdownloads/singlefile.php?cid=57&lid=4247";
+?>
